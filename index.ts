6001c36d0eb5ebd7b892f49551fad280ad5b1d63
mlyskawi <mlyskawi@stevens.edu>
2017-04-23 22:11:40 -0400

    Works for mutiple roots

--- index.ts
+++ index.ts
@@ -3,25 +3,25 @@
  */
 import { Queue } from './Queue';
 /*import * as fs from 'fs';
-import * as path from 'path';
+ import * as path from 'path';
 
 
-fs.readFile(path.join(__dirname,"infile"+".dat"),"utf-8", (err, file) => {
-    if (err) console.error("An error occurred while opening the file!", err);
-    else
-    {
-        TopologicalSort(graph(file));
+ fs.readFile(path.join(__dirname,"infile"+".dat"),"utf-8", (err, file) => {
+ if (err) console.error("An error occurred while opening the file!", err);
+ else
+ {
+ TopologicalSort(graph(file));
 
 
 
 
-    }
-});*/
+ }
+ });*/
 
 
-let input="4 3\n1 2\n0 1\n0 3";
-
-BFN(graph(input),0);
+let input="4 3\n1 2\n0 1\n3 1";
+let root=[0,3];
+BFN(graph(input),root);
 function graph(input1){
 
     let digraph = [];
@@ -60,13 +60,13 @@ function BFN (graph1,root1){
     for (let i=0;i < graph1.length;i++){
         visted.push(false)
     }
-
-
     let Q = new Queue([]);
-    Q.enqueue(graph1[root1]);
-    visted[root1] = true;
     let vector = [];
-    vector.push(root1);
+    for (let i=0;i < root1.length;i++){
+        Q.enqueue(graph1[root1[i]]);
+        visted[root1[i]] = true;
+        vector.push(root1[i]);
+    }
 
     while (Q.length != 0){
         let current = Q.dequeue();
@@ -92,4 +92,5 @@ function BFN (graph1,root1){
     }
 
 }
+
 
